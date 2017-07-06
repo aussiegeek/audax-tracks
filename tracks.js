6513d4732b4f8847f44efd071c347cdd53cce17e
Alan Harper <alan@aussiegeek.net>
2017-07-06 17:03:31 +1000

    Add support for BikeRouterToaster
    Did a lot of refactoring to make this work correctly. Now only stores
    one file per track too

--- tracks.js
+++ tracks.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 const fs = require('fs')
-const path = require('path')
 const URL = require('url')
 const axios = require('axios')
 const sha256 = require('sha.js')('sha256')
@@ -10,83 +9,100 @@ const topojson = require('topojson-server')
 const rides = JSON.parse(fs.readFileSync('perms.json'))
 const DOMParser = require('xmldom').DOMParser
 
-function filterPerm (urlString) {
-  if (typeof urlString !== 'string') return false
+function fetchRideWithGPS (url) {
+  const gpxURL = url + '.gpx?sub_format=track'
+  return axios.get(gpxURL)
+    .then(response => {
+      if (response.status === 404) {
+        return Promise.resolve(null)
+      }
+      return response.data
+    })
+}
 
-  const url = URL.parse(urlString)
+function fetchBikeRouteToaster (url) {
+  const id = /(?:Course.aspx\?course=|BRTWebUI\/Course\/)(\d+)/.exec(url)[1]
+
+  return axios.get('http://bikeroutetoaster.com/api/BRT.WebUI/Course/GetCourse/' + id)
+    .then(response => {
+      if (response.status === 404) {
+        console.log('Course not found')
+        return Promise.resolve(null)
+      }
+      return axios.post('http://bikeroutetoaster.com/BRTWebUI/Export/GPX', {data: response.data.CourseFile}).then(r => r.data)
+    })
+}
+
+function fetchGeoJSON (gpx) {
+  if (gpx === null) { return null }
+  const dom = new DOMParser()
+
+  const geojson = togeojson.gpx(dom.parseFromString(gpx, 'application/xml'))
+  return geojson
+}
+
+function fetchTopoJSON (geojson) {
+  if (geojson === null) { return null }
+  return topojson.topology({data: geojson})
+}
+
+function fetchGPX (trackURL) {
+  if (typeof trackURL !== 'string') return Promise.resolve(null)
+  const url = URL.parse(trackURL)
   switch (url.host) {
     case 'ridewithgps.com':
       const start = url.path.substring(0, 7)
-      return start === '/routes'
+      if (start === '/routes') {
+        return fetchRideWithGPS(trackURL)
+      }
+      return Promise.resolve(null)
+    case 'bikeroutetoaster.com':
+    case 'www.bikeroutetoaster.com':
+      return fetchBikeRouteToaster(trackURL)
+
     case 'audax.org.au':
     case 'www.audax.org.au':
     case 'www.google.com':
-      return false
+      return Promise.resolve(null)
     default:
-      return false
+      console.log('Unknown host ' + url.host)
+      return Promise.resolve(null)
   }
 }
 
-function fetchGPX (url) {
-  const gpxURL = url + '.gpx?sub_format=track'
-  const filename = 'data/' + sha256.update(url).digest('hex') + '.' + 'gpx'
+function downloadTrack (trackURL) {
+  if (trackURL == null) { return null }
+  const filename = 'data/' + sha256.update(trackURL).digest('hex') + '.' + 'topojson'
   if (fs.existsSync(filename)) {
     return new Promise((resolve, reject) => {
       fs.readFile(filename, (err, data) => {
         if (err) {
           console.log(err)
-          reject(err)
+          return reject(err)
         }
-        resolve(filename)
+        return resolve(filename)
       })
     })
-  } else {
-    return axios.get(gpxURL)
-      .then(response => {
-        if (response.status === 404) {
-          return null
-        }
-        fs.writeFileSync(filename, response.data)
-        return filename
-      })
   }
-}
-
-function fetchGeoJSON (gpxpath) {
-  const basename = path.basename(gpxpath, '.gpx')
-  const geojsonFilename = 'data/' + basename + '.geojson'
-  if (!fs.existsSync(geojsonFilename)) {
-    var gpx = new DOMParser().parseFromString(fs.readFileSync(gpxpath, 'utf8'))
-    const geojson = togeojson.gpx(gpx)
-
-    fs.writeFileSync(geojsonFilename, JSON.stringify(geojson))
-  }
-
-  return geojsonFilename
-}
-
-function fetchTopoJSON (geojsonpath) {
-  const basename = path.basename(geojsonpath, '.geojson')
-  const topojsonFilename = 'data/' + basename + '.topojson'
-  if (!fs.existsSync(topojsonFilename)) {
-    const geojson = JSON.parse(fs.readFileSync(geojsonpath, 'utf8'))
-    const topojsonObject = topojson.topology({data: geojson})
-    fs.writeFileSync(topojsonFilename, JSON.stringify(topojsonObject))
-  }
-  return topojsonFilename
-}
-
-function downloadPerm (url) {
-  return fetchGPX(url)
+  return fetchGPX(trackURL)
     .then(fetchGeoJSON)
     .then(fetchTopoJSON)
+    .then(topoJSON => {
+      return new Promise((resolve, reject) => {
+        fs.writeFile(filename, JSON.stringify(topoJSON), (err, data) => {
+          if (err) {
+            console.log(err)
+            return reject(err)
+          }
+          return resolve(filename)
+        })
+      })
+    })
 }
 
-function downloadTrack (ride) {
+function downloadTracks (ride) {
   return Promise.all(
-    ride.attachments.map(a => a.txtURL)
-      .filter(filterPerm)
-      .map(downloadPerm)
+    ride.attachments.map(a => downloadTrack(a.txtURL)).filter(data => data !== undefined)
   )
     .then(topojsonPaths => {
       if (topojsonPaths.length > 0) {
@@ -98,12 +114,14 @@ function downloadTrack (ride) {
           topoJson: topojsonPaths
         }
       } else {
-        console.log('No RideWithGPS links found for ' + ride.txtRideName)
+        console.log('No tracks links found for ' + ride.txtRideName)
+        console.log(ride.attachments)
+        console.log('------------------')
       }
     })
 }
 
-Promise.all(rides.map(downloadTrack))
+Promise.all(rides.map(downloadTracks))
   .then(perms => perms.filter(perm => perm != null))
   .then(perms => {
     fs.writeFileSync('data/perms.json', JSON.stringify(perms))
